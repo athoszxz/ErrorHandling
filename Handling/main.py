c3d2f545ec016d841f0823e40ec7a5732dd2ea3e
Athos Balmant <athos.kyoto@gmail.com>
2023-02-12 22:06:51 +0000

    Finalizado

--- Handling/main.py
+++ Handling/main.py
@@ -1,1 +1,22 @@
-print("Athos")
+def division_by_one(n: int) -> str:
+    try:
+        return str(1 / n)
+    except ZeroDivisionError:
+        return "Division by zero is not allowed"
+    except TypeError:
+        return "Only numbers are allowed"
+    except Exception:
+        return "Something went wrong"
+    finally:
+        print("\nAthos Balmant")
+
+
+def main():
+    print(division_by_one(2))
+    print(division_by_one(0))
+    print(division_by_one("0"))
+    print(division_by_one(1))
+
+
+if __name__ == "__main__":
+    main()
